eebc8649c0db6142b78e25bb222c43a2fc966658
Masaaki Matsubara <matsubara@noguchi.or.jp>
2024-10-14 15:07:57 +0900

    Added URL decode

--- src/test/java/ninja/mspp/io/mzml/test/MzmlTest.java
+++ src/test/java/ninja/mspp/io/mzml/test/MzmlTest.java
@@ -1,9 +1,12 @@
 package ninja.mspp.io.mzml.test;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLDecoder;
+
+import org.junit.Test;
 
 import io.github.msdk.MSDKException;
 import ninja.mspp.core.model.ms.Sample;
@@ -12,12 +15,12 @@ import ninja.mspp.io.mzml.MzmlReader;
 public class MzmlTest {
     private String test_file_path;
 	@Test
-	public void test() throws MSDKException {
+	public void test() throws MSDKException, UnsupportedEncodingException {
 		String file = "data/small.pwiz.1.1.mzML";
-                URL testFile = getClass().getClassLoader().getResource(file);
-                assertNotNull(testFile);
+		URL testFile = getClass().getClassLoader().getResource(file);
+		assertNotNull(testFile);
 		MzmlReader reader = new MzmlReader();
-                test_file_path = testFile.getPath();
+		test_file_path = URLDecoder.decode(testFile.getPath(), "UTF-8");
 		Sample sample = reader.read(test_file_path);
 	}
 
