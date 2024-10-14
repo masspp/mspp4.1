f0a98cdabaec38c53ad1bd48fdfb6151fd1204be
Masaki Murase <masaki@hautbois.jp>
2024-10-14 12:11:42 +0900

    added testdata into resouces folder

--- src/test/java/ninja/mspp/io/mzml/test/MzmlTest.java
+++ src/test/java/ninja/mspp/io/mzml/test/MzmlTest.java
@@ -1,17 +1,24 @@
 package ninja.mspp.io.mzml.test;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.net.URL;
 
 import io.github.msdk.MSDKException;
 import ninja.mspp.core.model.ms.Sample;
 import ninja.mspp.io.mzml.MzmlReader;
 
 public class MzmlTest {
+    private String test_file_path;
 	@Test
 	public void test() throws MSDKException {
-		String file = "/data/small.pwiz.1.1.mzML";
+		String file = "data/small.pwiz.1.1.mzML";
+                URL testFile = getClass().getClassLoader().getResource(file);
+                assertNotNull(testFile);
 		MzmlReader reader = new MzmlReader();
-		Sample sample = reader.read(file);
+                test_file_path = testFile.getPath();
+		Sample sample = reader.read(test_file_path);
 	}
 
 }
